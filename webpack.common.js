c4d6e170c3beea6b6bf7b78cf5df2f06a857249c
dev ak <ak@gmail.com>
2024-08-22 14:36:13 +0530

    updated code till html loader

--- webpack.common.js
+++ webpack.common.js
@@ -5,17 +5,19 @@ module.exports = {
   entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html"
+      template: "./src/template.html",
+      filename:"Movies.html"
+      
     })
   ],
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         use: [
           "style-loader", //3. Inject styles into DOM
-          "css-loader", //2. Turns css into commonjs
-          "sass-loader" //1. Turns sass into css
+          "css-loader" //2. Turns css into commonjs
+        
         ]
       }
     ]
